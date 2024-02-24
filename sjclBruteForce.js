ea89ae47972754d0e7c0ddd5482c218dd8f3646c
KAJIKK <62884492+KAJIKK@users.noreply.github.com>
2024-02-24 21:52:13 +0100

    Added some features

--- sjclBruteForce.js
+++ sjclBruteForce.js
@@ -1,16 +1,45 @@
-var sjcl = require('./sjcl.js')
-var encData = '{"iv":"UHbGpR7R8Dbw2WbwucGvFg==","v":1,"iter":10000,"ks":128,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"MQZ6b1wJ6/8=","ct":"bZBtJJoNtMmQNM34QOmEz5o3yjshIFZMu1Sd9XRyXXWPwR95Q9pGxi47lY9woXSoh31LbfUf"}'
-console.log("Here we go!")
-for (i=0;i<100;i++) {
-	var j = i.toString(10)
-	try{
-		var v1 = sjcl.decrypt(j, encData)
-		console.log("Password found!                  ")
-		console.log("Password: " + j)
-		console.log(v1)
-		break
-	}
-	catch{
-		process.stdout.write( "Trying: " + j + '\r')
-	}
+var sjcl = require('./sjcl.js');
+//var encData = '{"iv":"AaXqu3cWQAnZcCZNznQI9A==","v":1,"iter":1000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"9lJ1MmwnwyE=","ct":"XYWMvS9rJDj2KZ9mit8VqsN05ztiS1e2HOE7G7NMRsE="}';
+
+//var encData = '{"iv":"Gr5SQOQtJGuk/Fma2IcjrQ==","v":1,"iter":1000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"lz4WZ0Ndvs8=","ct":"7If5smLqq8+zS1Mj"}';
+
+var encData = '{"iv":"BjCTdk4oqNiIoGw7TI1U4g==","v":1,"iter":1000,"ks":256,"ts":64,"mode":"ccm","adata":"","cipher":"aes","salt":"/Ie4F+4cdjc=","ct":"UcHEXwudyRLzTCD5cPLewQ=="}';
+
+// Define the character set to generate combinations
+var characterSet = {
+    'a': 'abcdefghijklmnopqrstuvwxyz',
+    'A': 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+	'l': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
+    'n': '0123456789',
+	'*': 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+};
+
+var mask = "aAn"; // Example mask
+var minLength = mask.length;
+var maxLength = mask.length;
+
+console.log("Here we go!");
+
+generateCombinations("karel", 0);
+
+function generateCombinations(currentCombination, maskIndex) {
+    if (maskIndex === mask.length) {
+        try {
+            var decryptedData = sjcl.decrypt(currentCombination, encData);
+            console.log("Password found!");
+            console.log("Password: " + currentCombination);
+            console.log("Text: " + decryptedData);
+            process.exit(); // Terminate the script after finding the password
+        } catch (error) {
+            process.stdout.write("Trying: " + currentCombination + '\r');
+        }
+        return;
+    }
+
+    var currentMaskChar = mask[maskIndex];
+    var currentCharacterSet = characterSet[currentMaskChar];
+
+    for (var i = 0; i < currentCharacterSet.length; i++) {
+        generateCombinations(currentCombination + currentCharacterSet[i], maskIndex + 1);
+    }
 }
